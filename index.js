3697368b315a31ac245158a0113f7166a8242c39
Thiago <t.r.goveia@gmail.com>
2022-05-06 09:27:36 -0300

    .

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
+// Paulo Roberto Domingues dos Santos NUSP: 11838721
+// Thiago Ribeiro Goveia NUSP: 10835942
+
 const fs = require('fs')
 
 
 function extractList(string) {
-    const [_, ...rest] = string.split(' ')
+    const [_, ...rest] = string.split(' ') // serarar string por expaços para uam array desprezando o primeiro elemento
     return rest
 }
 function extractTransitionStates(string) {
-    return string.split(' ')
+    return string.split(' ') // separar string por espaços
 }
 
+// criar objeto de trnasições
 function buildTransitions(arrayOfTransitions, states) {
     const transitions = {}
     for (const transition of arrayOfTransitions) {
-        const [from, key ,to] = transition
-        if (!transitions[key]) {
-            transitions[key] = []
+        const [from, symbol ,to] = transition
+        // cada simbolo pode ter varias transições, então criar uma array e adicionar a transição à ela
+        if (!transitions[symbol]) {
+            transitions[symbol] = []
         }
-        transitions[key].push(states[parseInt(to)])
+        transitions[symbol].push(states[parseInt(to)]) // cada transição guarda a referencia para o estado de destino
     }
     return transitions
 }
 
+// criar objeto de estado
 function buildNode(acceptanceStates, node) {
     return {
         isAcceptance: acceptanceStates.includes(node),
@@ -29,11 +35,13 @@ function buildNode(acceptanceStates, node) {
 
 function buildStates(numberOfStates, acceptanceStates, transitionStates) {
     const states = []
+    // adicionar objetos de estado à array
     for (i = 0; i < numberOfStates; i++) {
         states.push(
             buildNode(acceptanceStates, i.toString())
         )
     }
+    // setar transições entre estados
     for (i = 0; i < numberOfStates; i++) {
         states[i].transitions = buildTransitions(
             transitionStates.filter(transition => transition[0] === i.toString()),
@@ -43,34 +51,39 @@ function buildStates(numberOfStates, acceptanceStates, transitionStates) {
     return states
 }
 
+// Ler arquivo e extrar array de cada linha
 const inputFile = fs.readFileSync('input.txt', 'utf8').split('\r\n')
 
-const numberOfStates = parseInt(inputFile[0])
-const terminalSymbols = extractList(inputFile[1])
-const acceptanceStates = extractList(inputFile[2])
-const numberOfTransitions = parseInt(inputFile[3])
-const transitionStates = inputFile.slice(4, 4 + numberOfTransitions).map(state => extractTransitionStates(state))
-const numberOfinputsIndex = 4 + numberOfTransitions
-const numberOfInputs = parseInt(inputFile[numberOfinputsIndex])
-const inputs = inputFile.slice(numberOfinputsIndex + 1, numberOfinputsIndex + numberOfInputs + 1)
-const states = buildStates(numberOfStates, acceptanceStates, transitionStates)
+const numberOfStates = parseInt(inputFile[0]) // extrair numero de estados
+const terminalSymbols = extractList(inputFile[1]) // extrair simbolos terminais
+const acceptanceStates = extractList(inputFile[2]) // extrair estados de aceitação
+const numberOfTransitions = parseInt(inputFile[3]) // extrair numero de transições
+const transitionStates = inputFile.slice(4, 4 + numberOfTransitions).map(state => extractTransitionStates(state)) // extrair transições
+const numberOfinputsIndex = 4 + numberOfTransitions // Calcular index do numero de inputs
+const numberOfInputs = parseInt(inputFile[numberOfinputsIndex]) // extrair numero de inputs
+const inputs = inputFile.slice(numberOfinputsIndex + 1, numberOfinputsIndex + numberOfInputs + 1) // extrair inputs
+const states = buildStates(numberOfStates, acceptanceStates, transitionStates) // construir grafo de estados estados
 
-// open file to write
 const outputFile = fs.openSync('output.txt', 'w')
 
+// percorrer grafo verificando aceitação
 const traverse = (state, symbols) => {
-    const [symbol, ...rest] = symbols
-    if (symbol === '-' && !state.isAcceptance) return false
-    if (symbol === '-' && state.isAcceptance) return true
-    if (!symbol && state.isAcceptance) return true
-    if (!symbol && !state.isAcceptance) return false
+    const [symbol, ...rest] = symbols  // separe a array de caracteres em cabeça | calda
+    if (symbol === '-' && !state.isAcceptance) return false // se cadeia vazia e não é estado de aceitação, rejeite
+    if (symbol === '-' && state.isAcceptance) return true // se cadeia vazia e é estado de aceitação, aceite
+    // se não temos um simbiolo, então toda a cadeia foi processada
+    if (!symbol && state.isAcceptance) return true  // se é estado de aceitação, aceite
+    if (!symbol && !state.isAcceptance) return false // se não é estado de aceitação, rejeite
+
+    // percorrer array de transições para o simbolo atual
     if (state.transitions[symbol]) {
         let isAcceptance = false
         for (const nextState of state.transitions[symbol]) {
-            isAcceptance = traverse(nextState, rest)
-            if (isAcceptance) return true
+            isAcceptance = traverse(nextState, rest) // percorrer grafo para cada transição
+            if (isAcceptance) return true // se um estado de aceitação for encontrado, aceite
         }
     }
+    // se houverem transições vazias, percorra grafo em buscar proximo simbolo
     if (state.transitions['-']) {
         let isAcceptance = false
         for (const nextState of state.transitions['-']) {
@@ -78,6 +91,7 @@ const traverse = (state, symbols) => {
             if (isAcceptance) return true
         }
     }
+    // se não existe transição para o simbolo atual, rejeite
     if (!state.transitions[symbol]) return false
 }
 
@@ -85,8 +99,8 @@ inputs.forEach(input => {
     const symbols = Array.from(input)
     let isAcceptance = traverse(states[0], symbols)
     if (isAcceptance) {
-        fs.writeSync(outputFile, `${input} aceita\n`)
+        fs.writeSync(outputFile, 'aceita\n')
     } else {
-        fs.writeSync(outputFile, `${input} rejeita\n`)
+        fs.writeSync(outputFile, 'rejeita\n')
     }
 })
